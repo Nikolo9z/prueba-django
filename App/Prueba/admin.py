6182c5640b17e389f6187dd339f81be26157049f
Nikolo <sanchez.nicolas.9917@gmail.com>
2022-03-31 23:30:11 -0300

    commit inicial

---
+++ App/Prueba/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from App.Prueba.models import *
+# Register your models here.
+admin.site.register(Estacionamiento)
---
+++ App/Prueba/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Estacionamiento(models.Model):
+    piso=models.CharField(primary_key=True,max_length=1)
+    estado=models.BooleanField(default=False)
+    tipos1=[
+        ('Vip','Vip'),
+        ('C','Colaborador'),
+        ('V','Visita')
+    ]
+    tipo1=models.CharField(max_length=3,choices=tipos1)
+    tipos2=[
+        ('A','Auto'),
+        ('B','Bus'),
+        ('M','Moto'),
+        ('C','Bicicleta')
+    ]
+    tipo2=models.CharField(max_length=3,choices=tipos2,null=True)
+    telefono=models.CharField(max_length=9)
+    
